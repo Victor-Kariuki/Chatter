76545e449d59512280d5028d8df74f8aef59dc63
Victor Kariuki <karizvic@gmail.com>
2020-04-14 13:06:08 +0300

    Create landing page

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,25 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Welcome to chatter.';
+  // selectors
+  var signInForm = querySelector('#signInForm');
+  var validationMessage = signInForm.querySelector('p.help');
+  var notificationBox = querySelector('.notification');
+  ButtonElement notificationButton = notificationBox.querySelector('.delete');
+  InputElement usernameField = signInForm.querySelector('input[type="text"]');
+  ButtonElement submitButton = signInForm.querySelector('button');
+
+  // event listeners
+  usernameField.addEventListener('input', (event) {
+    /**
+     * TODO: Run field vaildation
+     */
+  });
+
+  submitButton.addEventListener('click', (event) async {
+    // TODO: Run name field validation
+    // TODO: Submit name field to backend
+    // TODO: Handle success response
+    // TODO: Handle failure responses
+  });
 }
